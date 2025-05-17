f441e8f69bed2f2df2985b4ffe6783647338d825
Shadow3373 <nmani3373@gmail.com>
2025-05-17 16:55:32 +0530

    user active & inactive component created

---
+++ src/app/components/active-dialog/active-dialog.component.ts
@@ -0,0 +1,48 @@
+import { Component, inject } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { Router } from '@angular/router';
+import { AuthUserService } from '../../service/auth-user.service';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-active-dialog',
+  standalone: false,
+  templateUrl: './active-dialog.component.html',
+  styleUrl: './active-dialog.component.css',
+})
+export class ActiveDialogComponent {
+  dialogRef = inject(MatDialogRef<ActiveDialogComponent>);
+  data = inject(MAT_DIALOG_DATA);
+  payload = {
+    userCode: this.data.userCode,
+    userName: this.data.userName,
+    userStatus: this.data.userStatus,
+    reason: '',
+  };
+
+  constructor(private auth: AuthUserService) {}
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+
+  onActive(active: NgForm) {
+   if (this.payload.userStatus === "ACTIVE") {
+    const activeData = {
+      partyCode: this.payload.userCode,
+      reason: active.value.reason,
+    };
+    this.auth.inactiveCustomer(activeData).subscribe((res) => {
+      return res;
+    });
+   } else {
+    const activeData = {
+      partyCode: this.payload.userCode,
+      reason: active.value.reason,
+    };
+    this.auth.activeCustomer(activeData).subscribe((res) => {
+      return res;
+    });
+   }
+  }
+}
