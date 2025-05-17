f6ff6bb72f76a82621784f16436649928d947250
Shadow3373 <nmani3373@gmail.com>
2025-05-17 16:57:42 +0530

    added inactive & active and delete component

--- src/app/components/table/table.component.ts
+++ src/app/components/table/table.component.ts
@@ -1,7 +1,20 @@
-import { Component, Input, Output, ViewChild } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+  ViewChild,
+} from '@angular/core';
 import { AuthUserService } from '../../service/auth-user.service';
-import { MatPaginator, PageEvent } from '@angular/material/paginator';
+import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
+import {
+  MAT_DIALOG_DATA,
+  MatDialog,
+  MatDialogActions,
+  MatDialogContent,
+  MatDialogRef,
+} from '@angular/material/dialog';
+import { InactiveDialogComponent } from '../inactive-dialog/inactive-dialog.component';
 
 @Component({
   selector: 'app-table',
@@ -10,6 +23,7 @@ import { MatTableDataSource } from '@angular/material/table';
   templateUrl: 'table.component.html',
 })
 export class TableComponent {
+  dialog = inject(MatDialog);
   totalCount: any;
 
   displayedColumns: string[] = [
@@ -55,11 +69,47 @@ export class TableComponent {
     });
   }
 
+  inActiveDialog(customer: any) {
+    this.dialog.open(InactiveDialogComponent, {
+      width: '500px',
+      data: {
+        partyCode: customer.PARTY_CODE,
+        partyStatus: customer.ACTIVE_CODE,
+        partyName: customer.PARTY_NAME,
+      },
+    });
+  }
+
+  openDialog(customer: any): void {
+    this.dialog.open(DeleteDialogRefComponent, {
+      disableClose: false,
+      width: '250px',
+      data: {
+        user: customer,
+        list: this.dataSource,
+      },
+    });
+  }
+}
+
+@Component({
+  selector: 'app-delete-dialog',
+  templateUrl: './delete-dialogRef.component.html',
+  styleUrl: './delete-dialog.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [MatDialogContent, MatDialogActions],
+})
+export class DeleteDialogRefComponent {
+  customer = inject(MAT_DIALOG_DATA);
+  DialogRef = inject(MatDialogRef);
+
   remove(customer: any) {
-    const index = this.dataSource.data.indexOf(customer);
+    const datalist = this.customer.list.data;
+    const index = datalist.indexOf(this.customer.user);
     if (index > -1) {
-      this.dataSource.data.splice(index, 1);
-      this.dataSource.data = [...this.dataSource.data];
+      datalist.splice(index, 1);
+      this.customer.list.data = [...datalist];
+      console.log('customer deleted successfully');
     }
   }
 }
